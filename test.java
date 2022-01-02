ea90ce7a9a9a348bb674b110959cbd7e8194b8f6
BuildTools <unconfigured@null.spigotmc.org>
2022-01-02 23:37:19 +0900

    [테스트 중]

---
+++ test.java
@@ -0,0 +1,23 @@
+package javastudy;
+
+public class StudyJava0819_1 {
+	
+	public static void main(String[] args) {
+		
+		//문자열출력
+		System.out.println("안녕하세요. 자바공부합니다!");
+		//숫자출력
+		System.out.println(12345678);
+		//문자열출력
+		System.out.println("12345678");
+		System.out.println(100+100);//+로 만나면 결합
+	
+		System.out.println("100+100");
+		System.out.println(100+100+"100");
+		//""만나면 그 뒤는 문자가 됨,셈하려면 괄호 사용
+		System.out.println(100+100+"100"+(100+100));
+		System.out.println(100*100+"랄라"+100*100);
+		
+	}
+
+}
